061f89355029223b85fd8b67cb5091fa4e0bb99e
Don <don@ohspite.net>
2013-09-23 13:41:13 -0400

    implement options for printing tag dicts

--- xatag/tag_dict.py
+++ xatag/tag_dict.py
@@ -25,27 +25,55 @@ def tag_list_to_dict(tags):
             tag_dict[t.key].append(t.value)
         return tag_dict
 
-def print_tag_dict(tag_dict, prefix='', fsep=':', tsep=' ', out=sys.stdout):
+def print_tag_dict(tag_dict, prefix='', fsep=':', ksep=':', tsep=' ', 
+                   one_line=False, key_val_pairs=False, out=sys.stdout):
     """Print the tags for a file in a nice way."""
     def write_tag(key_name, dict_key):
         padding = max(1, longest_tag - len(key_name) + 1)
-        out.write(prefix + key_name + fsep + " "*padding)
-        out.write(tsep.join(map((lambda x: format_tag_value(x, tsep==' ')), sorted(tag_dict[dict_key]))))
-        out.write("\n")
+        sorted_vals = sorted(tag_dict[dict_key])
+        if (key_val_pairs and one_line):
+            do_quote_vals = False 
+        else:
+            do_quote_vals = (tsep==' ')
+        formatted_vals = map((lambda x: format_tag_value(x, do_quote_vals)), 
+                             sorted_vals)
+        if key_val_pairs:
+            if one_line: 
+                for val in formatted_vals:
+                    out.write(key_name + ksep + val + tsep)
+            else:
+                for val in formatted_vals:
+                    out.write(prefix + key_name + ksep + " "*padding + val + "\n")
+        else:
+            if one_line:
+                out.write(key_name + ksep)
+                out.write('"' + tsep.join(formatted_vals) + '" ')
+            else:
+                out.write(prefix + key_name + ksep + " "*padding)
+                out.write(tsep.join(formatted_vals))
+                out.write("\n")
         
-    longest_tag = 8     # TODO: compute this using the known tag list
+    # TODO: compute this using the known tag list, or the tags passed on the
+    # command line
+    longest_tag = 8     
+
+    if one_line and tag_dict and prefix: out.write(prefix)
     if '' in tag_dict:
         write_tag('tags', '')
     for k in sorted(tag_dict.keys()):
         if k=='' or k=='tags': continue
         write_tag(k, k)
+    if one_line and tag_dict and prefix: out.write("\n")
     if (not tag_dict) and prefix:
         out.write(prefix + "\n")
 
-def print_file_tags(fname, longest_filename=0, fsep=":", tsep=' ', out=sys.stdout, **unused):
+def print_file_tags(fname, longest_filename=0, fsep=":", ksep=':', tsep=' ', 
+                    one_line=False, key_val_pairs=False, 
+                    out=sys.stdout, **unused):
     padding = max(1, longest_filename - len(fname) + 1)
     prefix = fname + fsep + " "*padding
-    print_tag_dict(read_tags_as_dict(fname), prefix=prefix, fsep=fsep, tsep=tsep, out=out)
+    print_tag_dict(read_tags_as_dict(fname), prefix=prefix, fsep=fsep, ksep=ksep, tsep=tsep, 
+                   one_line=one_line, key_val_pairs=key_val_pairs, out=out)
 
 def merge_tags(tags1, tags2):
     """Merge the two tag dicts."""
