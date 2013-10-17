79175660c6fb03e0a5e546f48b36cee6b54146a1
Don <don@ohspite.net>
2013-10-17 09:18:26 -0400

    set initial version number to 0.1.0-dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
 import setuptools
 setup(name='xatag',
-      version='0.0.0',
+      version='0.1.0-dev',
       description='Tag any file using extended attributes',
       author='Don',
       author_email='don@ohspite.net',
