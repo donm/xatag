9e08a258492332f7e3d8ca39e63d229da28120ac
Don <don@ohspite.net>
2013-10-17 00:38:04 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from distutils.core import setup
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
 import setuptools
 setup(name='xatag',
       version='0.0.0',
-      py_modules=['xatag'],
+      description='Tag any file using extended attributes',
+      author='Don',
+      author_email='don@ohspite.net',
+      url='http://xatag.org',
+      packages=['xatag'],
+      scripts=['bin/xatag'],
+      install_requires=['docopt', 'xattr'],
+      tests_require=['pytest']
       )
